fe82605a5fc11cf2b447718f8976974bc068896a
Josh Horowitz <joshhorowitz@lyft.com>
2021-12-04 18:13:00 -0800

    scouting is a hybrid

--- bot.js
+++ bot.js
@@ -105,21 +105,21 @@ function spam(target, msg) {
 function getPlacementString(placement) {
     switch (placement) {
         case 1:
-            return '1st place PogChamp';
+            return '1st PogChamp';
         case 2:
-            return '2th place VoHiYo ';
+            return '2th VoHiYo ';
         case 3:
-            return '3rd place TTours';
+            return '3rd TTours';
         case 4:
-            return '4th place Kappa';
+            return '4th Kappa';
         case 5:
-            return '5th place MrDestructoid';
+            return '5th MrDestructoid';
         case 6:
-            return '6th place NotLikeThis';
+            return '6th NotLikeThis';
         case 7:
-            return '7th place BibleThump BibleThump';
+            return '7th BibleThump BibleThump';
         case 8:
-            return '8th place BibleThump BibleThump BibleThump BibleThump';
+            return '8th BibleThump BibleThump BibleThump BibleThump';
     }
 
 }
@@ -140,9 +140,13 @@ function getDamageString(damage) {
 }
 
 function getHistoryMessage(matchData) {
+    return getHistoryMessageWithPUUID(matchData, PUUID);
+}
+
+function getHistoryMessageWithPUUID(matchData, puuid) {
     if (!matchData.info)
         return bugString();
-    const fan = matchData.info.participants.find(p => p.puuid === PUUID);
+    const fan = matchData.info.participants.find(p => p.puuid === puuid);
     if (!fan)
         return bugString();
     let comp = '';
@@ -163,7 +167,7 @@ function getHistoryMessage(matchData) {
             itemStr = ` (${unit.items.map(getName)})`;
         }
     });
-    var match = `Last Game twitch.tv/anathana placed ${getPlacementString(fan.placement)}. `;
+    var match = `Last Game was a ${getPlacementString(fan.placement)}. `;
     match += `He dealt ${fan.total_damage_to_players} damage to players that game ${getDamageString(fan.total_damage_to_players)}. `;
     if (carries.includes('Malz'))
         match += 'He ran TwitchLit MALZ REROLL TwitchLit. ';
@@ -235,7 +239,7 @@ function onMessageHandler(target, context, msg, self) {
                 const req2 = res.on('data', d => {
                     history = JSON.parse(d);
                     const mostRecent = history[0];
-                    MATCH_DATA = `https://americas.api.riotgames.com/tft/match/v1/matches/${mostRecent}?api_key=${API_KEY}`
+                    MATCH_DATA = `https://americas.api.riotgames.com/tft/match/v1/matches/${mostRecent}?api_key=${API_KEY}`;
                     https.get(MATCH_DATA, res2 => {
                         let body = '';
                         res2.on('data', function (chunk) {
@@ -258,29 +262,47 @@ function onMessageHandler(target, context, msg, self) {
             URL = `https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-name/${enemy}?api_key=${API_KEY}`;
             const req = https.get(URL, res => {
                 res.on('data', d => {
-                    console.log('here');
-                    summonerID = JSON.parse(d).id;
+                    const parsed = JSON.parse(d);
+                    summonerID = parsed.id;
+                    summonerPUUID = parsed.puuid;
                     if (!summonerID) {
-                        console.log('no summoner id');
                         client.say(target, bugString());
-                        return
+                        return;
                     }
                     MMR_URL = `https://na1.api.riotgames.com/tft/league/v1/entries/by-summoner/${summonerID}?api_key=${API_KEY}`;
-                    const req2 = https.get(MMR_URL, res2 => {
-                        res2.on('data', d => {
-                            console.log('there');
-                            fan = JSON.parse(d.toString())[0];
-                            if (!fan || !fan.summonerName) {
-                                client.say(target, `BibleThump ${commandName.split('scout ')[1]} hasn't played any ranked games this set FeelsBadMan BibleThump`);
-                                return
-                            }
-                            summoner = `${fan.summonerName} is currently ${fan.tier} ${fan.rank} ${fan.leaguePoints} LP.\n\n`;
-                            winrate = Number((fan.wins / (fan.wins + fan.losses) * 100).toFixed(1));
-                            summoner += `This set they have gotten first ${fan.wins} out of ${fan.wins + fan.losses} games (${winrate}%). `;
-                            client.say(target, summoner);
+                    const HISTORY_URL = `https://americas.api.riotgames.com/tft/match/v1/matches/by-puuid/${summonerPUUID}/ids?count=20&api_key=${API_KEY}`
+
+                    console.log(JSON.parse(d));
+
+                    const urls = [MMR_URL, HISTORY_URL];
+                    console.log(urls);
+                    Promise.all(urls.map(url => request(url))).then((data) => {
+
+                        const mmrData = JSON.parse(data[0])[0];
+                        console.log(mmrData);
+                        if (!mmrData || !mmrData.summonerName) {
+                            client.say(target, `BibleThump ${commandName.split('scout ')[1]} hasn't played any ranked games this set FeelsBadMan BibleThump`);
+                            return;
+                        }
+                        summoner = `${mmrData.summonerName} is currently ${mmrData.tier} ${mmrData.rank} ${mmrData.leaguePoints} LP.\n\n`;
+                        winrate = Number((mmrData.wins / (mmrData.wins + mmrData.losses) * 100).toFixed(1));
+                        summoner += `This set they have gotten first ${mmrData.wins} out of ${mmrData.wins + mmrData.losses} games (${winrate}%). `;
+
+                        const mostRecent = JSON.parse(data[1].toString())[0];
+                        MATCH_DATA = `https://americas.api.riotgames.com/tft/match/v1/matches/${mostRecent}?api_key=${API_KEY}`;
+                        console.log(MATCH_DATA);
+                        https.get(MATCH_DATA, res2 => {
+                            let body = '';
+                            res2.on('data', function (chunk) {
+                                body += chunk; // string conversion
+                            }).on('end', function () {
+                                matchData = JSON.parse(body);
+                                client.say(target, summoner + getHistoryMessageWithPUUID(matchData, summonerPUUID));
+                            });
+                            res2.on('error', error => console.error(error));
                         });
-                    })
-                })
+                    });
+                });
             });
         } else if (commandName.includes('!MMR')) {
             checkRiotAvailable(client, target);
@@ -331,7 +353,8 @@ function onMessageHandler(target, context, msg, self) {
             spam(target, msg);
         }
 
-    } catch (error) {
+    } catch
+        (error) {
         console.log(error);
         client.say(target, bugString());
     }
