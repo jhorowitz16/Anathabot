bb28bfc5dc0df961f20913b9c6d204b50a7f8f8c
Josh Horowitz <joshua.horowitz@instacart.com>
2022-05-26 11:24:16 -0700

    tutorial part 1

--- leaderboard/config/routes.rb
+++ leaderboard/config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "/articles", to: "articles#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+
+
+Rails.application.routes.draw do
+  root "articles#index"
+
+  resources :articles
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
 end
